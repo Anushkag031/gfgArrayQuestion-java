2b2490b67d3c848c62eabb50b0acf535072b37f2
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2023-09-01 19:13:59 +0530

    Update Find theElementThatAppearsOnce.java

--- Find theElementThatAppearsOnce.java
+++ Find theElementThatAppearsOnce.java
@@ -1,1 +1,17 @@
+problem link:  https://practice.geeksforgeeks.org/problems/element-appearing-once2552/0
 
+//User function Template for Java
+
+class Sol
+{
+    public static int search(int A[], int N)
+    {
+        // your code here
+        int ans=0;
+        for(int i=0; i<N; i++)
+        {
+            ans=ans^A[i];  //performing XOR, it cancels same elements
+        }
+        return ans;
+    }
+}
