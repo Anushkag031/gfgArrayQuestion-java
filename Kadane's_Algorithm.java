5bbe4369f1086b6b857cd74b93d4bbe1387a99a1
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2023-09-03 19:08:14 +0530

    Update Kadane's_Algorithm.java

--- Kadane's_Algorithm.java
+++ Kadane's_Algorithm.java
@@ -1,1 +1,28 @@
 
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long maxsum=arr[0];
+        long cursum=arr[0];
+        for(int i=1; i<arr.length; i++)
+        {
+             if(cursum<0)
+            {
+                cursum=0;
+            }
+            cursum+=arr[i];
+            maxsum=Math.max(maxsum,cursum);
+           
+           
+        }
+        return maxsum;
+        
+    }
+    
+}
