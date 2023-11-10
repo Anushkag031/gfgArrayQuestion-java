5f174e8174eb6706e743b6bdcac2d4be9ecd4345
Anushka gupta <104565993+Anushkag031@users.noreply.github.com>
2023-11-10 12:00:50 +0530

    Create count-odd-even.java

---
+++ count-odd-even.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public void countOddEven(int[] arr, int n)
+    {
+        // Code here
+        int even =0;
+        int odd=0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]%2==0)
+            {
+                even++;
+            }
+            else
+            odd++;
+        }
+        System.out.println(odd+" "+even);
+    }
+}
